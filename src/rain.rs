9f21d5184fd8f102aa22739ea39f90a296e290a9
Pontus Horn <pontus.horn@finch3d.com>
2023-12-04 23:18:58 +0100

    Nicer rain splashing

--- src/rain.rs
+++ src/rain.rs
@@ -1,3 +1,5 @@
+use std::f32::consts::PI;
+
 use crate::{
     level::Level,
     velocity::{update_position, Velocity},
@@ -30,8 +32,9 @@ impl Plugin for RainPlugin {
 }
 
 const DENSITY: i8 = 3;
-const HEIGHT: f32 = 16.;
-const VELOCITY: Vec2 = Vec2::new(50., -800.);
+const LENGTH: f32 = 16.;
+const ANGLE: f32 = -1.4;
+const SPEED: f32 = 800.;
 
 fn spawn_rain(mut commands: Commands, camera_query: Query<&OrthographicProjection>) {
     let mut rng = thread_rng();
@@ -42,36 +45,41 @@ fn spawn_rain(mut commands: Commands, camera_query: Query<&OrthographicProjectio
             .spawn(SpriteBundle {
                 sprite: Sprite {
                     color: Color::WHITE,
-                    custom_size: Some(Vec2::new(1., HEIGHT)),
-                    anchor: Anchor::BottomCenter,
+                    custom_size: Some(Vec2::new(LENGTH, 1.)),
                     ..default()
                 },
-                transform: Transform::from_translation(Vec3::new(
-                    rng.gen_range(camera_projection.area.min.x..camera_projection.area.max.x),
-                    camera_projection.area.max.y,
-                    2.,
-                ))
-                .with_rotation(Quat::from_rotation_z(0.2)),
+                transform: Transform::from_rotation(Quat::from_rotation_z(ANGLE)).with_translation(
+                    Vec3::new(
+                        rng.gen_range(camera_projection.area.min.x..camera_projection.area.max.x),
+                        camera_projection.area.max.y,
+                        2.,
+                    ),
+                ),
                 ..default()
             })
-            .insert(Velocity(VELOCITY))
+            .insert(Anchor::CenterRight)
+            .insert(Velocity(Vec2::from_angle(ANGLE) * SPEED))
             .insert(Rain(RainState::Falling));
     }
 }
 
 fn splash_rain(mut rain_query: Query<(&mut Rain, &mut Velocity, &mut Transform)>) {
+    let rng = &mut thread_rng();
     for (mut rain, mut rain_velocity, mut rain_transform) in rain_query.iter_mut() {
         match rain.0 {
             RainState::Falling => {
-                if rain_transform.translation.y < Level::GROUND_Y {
+                if rain_transform.translation.y <= Level::GROUND_Y {
                     rain.0 = RainState::Splashing;
-                    rain_velocity.0.y *= -1.;
-                    rain_transform.scale.y *= 0.7;
-                    rain_transform.rotate_z(-0.4);
+                    rain_transform.translation.y = Level::GROUND_Y;
+                    rain_transform.scale.x *= rng.gen_range(0.4..0.8);
+                    let splash_angle = PI / 2. + rng.gen_range(-0.6..0.6);
+                    let splash_speed = SPEED * rng.gen_range(0.2..0.8);
+                    rain_transform.rotate_local_z(splash_angle - ANGLE);
+                    rain_velocity.0 = Vec2::from_angle(splash_angle) * splash_speed;
                 }
             }
             RainState::Splashing => {
-                rain_transform.scale.y *= 0.5;
+                rain_transform.scale.y *= 0.7;
             }
         }
     }
